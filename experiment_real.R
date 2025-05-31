9d8b9f71617b1577fc63759e4b19d36394b4a7e9
Allen Kei <allen29@ucla.edu>
2025-05-31 15:48:21 -0700

    update

--- experiment_real.R
+++ experiment_real.R
@@ -11,24 +11,37 @@ source("EVAL.R")
 ######################
 
 data("MITphone")
-result <- CPD_STERGM(MITphone, directed=FALSE, network_stats=c("edges", "isolates", "triangles"))
+
+
+node_degree <- rep(0, dim(MITphone[[1]])[1])
+for (t in seq_len(length(MITphone))) {
+  mat <- MITphone[[t]]
+  node_degree <- node_degree + rowSums(mat)
+}; rm(mat)
+node_status <- ifelse(node_degree > median(node_degree), "H", "L")
+
+
+result <- CPD_STERGM(MITphone, directed=FALSE, network_stats=c("edges", "isolates", "triangles", "nodematch(\"node_attr\")"), 
+                     node_attr=node_status, list_of_lambda=10^c(0:4), threshold_alpha=0.025)
 
 
 gSeg_result <- Evaluation_gSeg_on_stats(MITphone, p_threshold=0.05, num_stats=3, is_experiment=TRUE)
 kerSeg_result <- Evaluation_kerSeg_on_stats(MITphone, p_threshold=0.001, num_stats=3, is_experiment=TRUE)
+rdpg_result <- Evaluation_RDPG(MITphone, M=100, d=5, delta=5, is_experiment=TRUE)
+nbs_result <- Evaluation_NBS(MITphone, M=15, delta=7, is_experiment=TRUE)
 
 
 theta_change <- result$theta_change; threshold <- result$threshold; xtick <- result$est_CP
 seq_date <- seq(as.Date("2004-09-15"), as.Date("2005-05-04"), by="days"); tau <- length(seq_date)-1
 
 
-par(mar=c(4, 4, 2, 1), fig=c(0,1,0,0.74))
+par(mar=c(4, 4, 2, 1), fig=c(0,1,0,0.66))
 plot(1:length(theta_change), theta_change, type='l',ylab="", xlab="", xaxt="n", yaxt="n")
+title(ylab="Change magnitude")
 abline(h = threshold, col='red',lwd=2)
 
 # winter and spring vacation (duration): seq_date[95];seq_date[110];seq_date[188];seq_date[192]
 win_spr_break <- c(95,110,188,192)
-
 for(i in c(1,3)){
   rect(xleft = win_spr_break[i]-2, xright = win_spr_break[i+1]-2, ybottom = par("usr")[3], ytop = par("usr")[4], 
        border = NA, col = adjustcolor("grey", alpha = 0.4))
@@ -36,27 +49,38 @@ for(i in c(1,3)){
 
 
 axis(side=1, at=xtick-2, labels = F, lwd = 0, lwd.ticks = 1)
-text(x=xtick-8,  par("usr")[3]-1.8, labels = seq_date[xtick], srt=45, cex=0.7, xpd=TRUE)
+text(x=xtick-8,  par("usr")[3]-2, labels = seq_date[xtick], srt=45, cex=0.7, xpd=TRUE)
 
 ytick <- c(0,2,4,6,8,10)
 axis(side=2, at=ytick, labels = FALSE)
 text(par("usr")[1]-1.7, ytick, labels=ytick, pos=2, xpd=TRUE, cex=0.8)
 
 
+
+par(mar=c(0, 4, 0, 1), fig=c(0,1,0.59,0.66), new=T)
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in nbs_result){abline(v=i-1, col='blue', lwd=2)}
+text(par("usr")[1]+1, 0.45, labels='CPDnbs', pos=2, xpd=TRUE, cex=0.8)
+
 par(mar=c(0, 4, 0, 1), fig=c(0,1,0.67,0.74), new=T)
-plot(NULL, ylim=c(0,1), xlim=c(1,tau-1), ylab="", xlab="", xaxt="n", yaxt="n")
-for(i in result$est_CP){abline(v=i-2, col='red', lwd=2)}
-text(par("usr")[1]+1, 0.45, labels='CPDstergm', pos=2, xpd=TRUE, cex=0.8)
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in rdpg_result){abline(v=i-1, col='blue', lwd=2)}
+text(par("usr")[1]+1, 0.45, labels='CPDrdpg', pos=2, xpd=TRUE, cex=0.8)
 
 par(mar=c(0, 4, 0, 1), fig=c(0,1,0.75,0.82), new=T)
-plot(NULL, ylim=c(0,1), xlim=c(1,tau-1), ylab="", xlab="", xaxt="n", yaxt="n")
-for(i in kerSeg_result){abline(v=i-2, col='blue', lwd=2)}
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in kerSeg_result){abline(v=i-1, col='blue', lwd=2)}
 text(par("usr")[1]+1, 0.45, labels='kerSeg', pos=2, xpd=TRUE, cex=0.8)
 
-par(mar=c(0, 4, 0, 1), fig=c(0,1,0.83,0.90), new=T)
-plot(NULL, ylim=c(0,1), xlim=c(1,tau-1), ylab="", xlab="", xaxt="n", yaxt="n")
-for(i in gSeg_result){abline(v=i-2, col='blue', lwd=2)}
+par(mar=c(0, 4, 0, 1), fig=c(0,1,0.83,0.9), new=T)
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in gSeg_result){abline(v=i-1, col='blue', lwd=2)}
 text(par("usr")[1]+1, 0.45, labels='gSeg', pos=2, xpd=TRUE, cex=0.8)
+
+
+
+MIT_result <- list(result$est_CP, gSeg_result, kerSeg_result, rdpg_result, nbs_result)
+#save(MIT_result, file = 'MITphone_result.Rdata') # Saved figure: 8 by 5
 
 
 
@@ -75,7 +99,6 @@ rownames(market) <- date_vec
 start <- which(date_vec == '2007-01-01')
 end <- which(date_vec == '2010-01-04')
 date_range <- start:end
-mydata <- array(NA,c(29, 29, length(date_range)))
 df <- list()
 
 for(i in 1:length(date_range)){
@@ -83,22 +106,35 @@ for(i in 1:length(date_range)){
   temp <- ifelse(cor(temp)< 0, 1, 0)
   diag(temp) <- 0
   df[[i]] <- temp
-}
+}; rm(DJIA, temp)
 
 
-result <- CPD_STERGM(df, directed=FALSE, network_stats=c("edges", "triangles"))
+node_degree <- rep(0, dim(df[[1]])[1])
+for (t in seq_len(length(df))) {
+  mat <- df[[t]]
+  node_degree <- node_degree + rowSums(mat)
+}; rm(mat)
+node_status <- ifelse(node_degree > median(node_degree), "H", "M")
+
+
+result <- CPD_STERGM(df, directed=FALSE, network_stats=c("edges", "triangles", "nodematch(\"node_attr\")"), 
+                     node_attr=node_status, list_of_lambda=10^c(0:4), threshold_alpha=0.025)
+
 
 gSeg_result <- Evaluation_gSeg(df, p_threshold=0.001, is_experiment=TRUE)
 kerSeg_result <- Evaluation_kerSeg(df, p_threshold=0.001, is_experiment=TRUE)
+rdpg_result <- Evaluation_RDPG(df, M=100, d=5, delta=5, is_experiment=TRUE)
+nbs_result <- Evaluation_NBS(df, M=15, delta=7, is_experiment=TRUE)
 
 
 
 theta_change <- result$theta_change; threshold <- result$threshold; xtick <- result$est_CP
 seq_date <- rownames(market[start:end,]); tau <- length(seq_date)-1
 
-par(mar=c(4, 4, 2, 1), fig=c(0,1,0,0.74))
+par(mar=c(4, 4, 2, 1), fig=c(0,1,0,0.66))
 plot(1:length(theta_change), theta_change, type='l',ylab="", xlab="", xaxt="n", yaxt="n")
 abline(h = threshold, col='red',lwd=2)
+title(ylab="Change magnitude")
 axis(side=1, at=xtick-2, labels = F, lwd = 0, lwd.ticks = 1)
 text(x=xtick-6,  par("usr")[3]-1.3, labels = seq_date[xtick], srt=45, cex=0.7, xpd=TRUE)
 
@@ -107,77 +143,30 @@ axis(side=2, at=ytick, labels = FALSE)
 text(par("usr")[1]-1.7, ytick, labels=ytick, pos=2, xpd=TRUE, cex=0.8)
 
 
+par(mar=c(0, 4, 0, 1), fig=c(0,1,0.59,0.66), new=T)
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in nbs_result){abline(v=i-1, col='blue', lwd=2)}
+text(par("usr")[1]+1, 0.45, labels='CPDnbs', pos=2, xpd=TRUE, cex=0.8)
+
 par(mar=c(0, 4, 0, 1), fig=c(0,1,0.67,0.74), new=T)
-plot(NULL, ylim=c(0,1), xlim=c(1,tau-1), ylab="", xlab="", xaxt="n", yaxt="n")
-for(i in result$est_CP){abline(v=i-2, col='red', lwd=2)}
-text(par("usr")[1]+1, 0.45, labels='CPDstergm', pos=2, xpd=TRUE, cex=0.8)
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in rdpg_result){abline(v=i-1, col='blue', lwd=2)}
+text(par("usr")[1]+1, 0.45, labels='CPDrdpg', pos=2, xpd=TRUE, cex=0.8)
 
 par(mar=c(0, 4, 0, 1), fig=c(0,1,0.75,0.82), new=T)
-plot(NULL, ylim=c(0,1), xlim=c(1,tau-1), ylab="", xlab="", xaxt="n", yaxt="n")
-for(i in kerSeg_result){abline(v=i-2, col='blue', lwd=2)}
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in kerSeg_result){abline(v=i-1, col='blue', lwd=2)}
 text(par("usr")[1]+1, 0.45, labels='kerSeg', pos=2, xpd=TRUE, cex=0.8)
 
-par(mar=c(0, 4, 0, 1), fig=c(0,1,0.83,0.90), new=T)
-plot(NULL, ylim=c(0,1), xlim=c(1,tau-1), ylab="", xlab="", xaxt="n", yaxt="n")
-for(i in gSeg_result){abline(v=i-2, col='blue', lwd=2)}
+par(mar=c(0, 4, 0, 1), fig=c(0,1,0.83,0.9), new=T)
+plot(NULL, ylim=c(0,1), xlim=c(1,tau), ylab="", xlab="", xaxt="n", yaxt="n")
+for(i in gSeg_result){abline(v=i-1, col='blue', lwd=2)}
 text(par("usr")[1]+1, 0.45, labels='gSeg', pos=2, xpd=TRUE, cex=0.8)
 
 
-
-
-library(tergm)
-y_data <- df
-
-y_before93 <- y_after93 <- y_after121 <- list()
-holder_after93 <- holder_before93 <- holder_after121 <- c()
-duration <- 24
-
-
-for(i in 1:duration){
-  holder_before93 <- c(holder_before93, 93-(duration+1-i))
-  y_before93[[i]] <- as.network(y_data[[93-(duration+1-i)]], directed = F)
-  
-  holder_after93 <- c(holder_after93,93-1+i)
-  y_after93[[i]] <- as.network(y_data[[93-1+i]], directed = F)
-  
-  holder_after121 <- c(holder_after121, 121-1+i)
-  y_after121[[i]] <- as.network(y_data[[121-1+i]], directed = F)
-}
-
-
-before93.dyn <- NetSeries(y_before93)
-after93.dyn <- NetSeries(y_after93)
-after121.dyn <- NetSeries(y_after121)
-
-before93.fit <- tergm(
-  before93.dyn ~
-    Form(~edges + triangles) +
-    Persist(~edges + triangles),
-  estimate = "CMLE",
-  times = c(1:duration)
-)
-
-
-after93.fit <- tergm(
-  after93.dyn ~
-    Form(~edges + triangles) +
-    Persist(~edges + triangles),
-  estimate = "CMLE",
-  times = c(1:duration)
-)
-
-
-after121.fit <- tergm(
-  after121.dyn ~
-    Form(~edges + triangles) +
-    Persist(~edges + triangles),
-  estimate = "CMLE",
-  times = c(1:duration)
-)
+stock_result <- list(result$est_CP, gSeg_result, kerSeg_result, rdpg_result, nbs_result)
+#save(stock_result, file = 'Stock_result.Rdata') # Saved figure: 8 by 5
 
 
 
-summary(before93.fit)
-summary(after93.fit)
-summary(after121.fit)
 
--- plot.R
+++ plot.R
@@ -205,3 +205,111 @@ ggplot(raster_data[raster_data$value == 1, ], aes(x = time, y = as.numeric(facto
   geom_vline(xintercept = change_points, color = "red", linewidth = 0.5)
 
 
+
+###################
+# Time Comparison #
+###################
+
+library(CPDstergm)
+source("EVAL.R")
+
+num_node <- c(50, 100, 200)
+network_stats=c("edges", "mutual")
+SBM_time_by_node <- matrix(NA, nrow=7, ncol=3)
+
+for(i in 1:length(num_node)){
+  set.seed(1)
+  SBM_list <- sim_SBM_list(num_seq=3, n=num_node[i], rho=0.0)
+  
+  time1 <- system.time( CPD_STERGM_list(SBM_list, directed=TRUE, network_stats, list_of_lambda=100) )
+  
+  time2 <- system.time( Evaluation_gSeg(SBM_list, p_threshold=0.05) )
+  
+  time3 <- system.time( Evaluation_kerSeg(SBM_list, p_threshold=0.001) )
+  
+  time4 <- system.time( Evaluation_gSeg_on_stats(SBM_list, p_threshold=0.05, num_stats=length(network_stats)) )
+  
+  time5 <- system.time( Evaluation_kerSeg_on_stats(SBM_list, p_threshold=0.001, num_stats=length(network_stats)) )
+  
+  time6 <- system.time( Evaluation_RDPG(SBM_list, M=50, d=5, delta=5) )
+  
+  time7 <- system.time( Evaluation_NBS(SBM_list, M=15, delta=5) )
+  
+  SBM_time_by_node[,i] <- c(time1['elapsed'],time2['elapsed'],time3['elapsed'],time4['elapsed'],
+                            time5['elapsed'],time6['elapsed'],time7['elapsed'])
+  
+}
+
+#save(SBM_time_by_node, file = 'SBMrho0_time.Rdata')
+
+
+
+
+
+num_node <- c(50, 100, 200)
+y1_target <- c(250, 500, 1000)
+network_stats=c("edges", "mutual")
+coefs_pos <- matrix(c(-1, -1, -1, -1, -2, 1, -2, 1), nrow=2, ncol=4, byrow = T)
+coefs_neg <- matrix(c( -1, -1, -1, -1, -2, -1, -2, -1), nrow=2, ncol=4, byrow = T)
+
+
+STERGM_time_by_node <- matrix(NA, nrow=7, ncol=3)
+
+for(i in 1:length(num_node)){
+  set.seed(1)
+  STERGM_list <- sim_STERGM_list(num_seq=3, n=num_node[i], network_stats, coefs_pos, coefs_neg, y1_stats=y1_target[i])
+  
+  time1 <- system.time( CPD_STERGM_list(STERGM_list, directed=TRUE, network_stats, list_of_lambda=100) )
+  
+  time2 <- system.time( Evaluation_gSeg(STERGM_list, p_threshold=0.05) )
+  
+  time3 <- system.time( Evaluation_kerSeg(STERGM_list, p_threshold=0.001) )
+  
+  time4 <- system.time( Evaluation_gSeg_on_stats(STERGM_list, p_threshold=0.05, num_stats=length(network_stats)) )
+  
+  time5 <- system.time( Evaluation_kerSeg_on_stats(STERGM_list, p_threshold=0.001, num_stats=length(network_stats)) )
+  
+  time6 <- system.time( Evaluation_RDPG(STERGM_list, M=50, d=5, delta=5) )
+  
+  time7 <- system.time(Evaluation_NBS(STERGM_list, M=15, delta=5) )
+  
+  STERGM_time_by_node[,i] <- c(time1['elapsed'],time2['elapsed'],time3['elapsed'],time4['elapsed'],
+                               time5['elapsed'],time6['elapsed'],time7['elapsed'])
+  
+}
+
+#save(STERGM_time_by_node, file = 'STERGMp4_time.Rdata')
+
+
+
+
+num_node <- c(50, 100, 200)
+network_stats=c("edges", "mutual")
+RDPGM_time_by_node <- matrix(NA, nrow=7, ncol=3)
+
+for(i in 1:length(num_node)){
+  set.seed(1)
+  RDPG_list <- sim_RDPG_list(num_seq = 3, n = num_node[i], rho = 0.9, d = 20)
+  
+  time1 <- system.time( CPD_STERGM_list(RDPG_list, directed=TRUE, network_stats, list_of_lambda=10) )
+  
+  time2 <- system.time( Evaluation_gSeg(RDPG_list, p_threshold=0.05) )
+  
+  time3 <- system.time( Evaluation_kerSeg(RDPG_list, p_threshold=0.001) )
+  
+  time4 <- system.time( Evaluation_gSeg_on_stats(RDPG_list, p_threshold=0.05, num_stats=length(network_stats)) )
+  
+  time5 <- system.time( Evaluation_kerSeg_on_stats(RDPG_list, p_threshold=0.001, num_stats=length(network_stats)) )
+  
+  time6 <- system.time( Evaluation_RDPG(RDPG_list, M=50, d=5, delta=5) )
+  
+  time7 <- system.time( Evaluation_NBS(RDPG_list, M=15, delta=5) )
+  
+  RDPGM_time_by_node[,i] <- c(time1['elapsed'],time2['elapsed'],time3['elapsed'],time4['elapsed'],
+                              time5['elapsed'],time6['elapsed'],time7['elapsed'])
+  
+}
+
+#save(RDPGM_time_by_node, file = 'RDPGMd20_time.Rdata')
+
+
